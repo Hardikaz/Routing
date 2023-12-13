d176114ac3677e890c4bd0791c1fb93580c7272d
Hardik Sharma <sharmahardikaz@gmail.com>
2023-12-13 22:13:39 +0530

    ButtonRouting

--- routing-app/src/App.js
+++ routing-app/src/App.js
@@ -1,12 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './components/Home'
 import { About } from './components/About'
+import { Navbar} from './components/Navbar'
+import { OrderConfirmed } from './components/OrderConfirmed'
 function App() {
   return (
+    <>
+    <Navbar />
     <Routes>
            <Route path='/' element={<Home />} />
            <Route path='about' element={<About />} />
+           <Route path='order-summary' element={<OrderConfirmed />}></Route>
     </Routes>
+    </>
   );
 }
 
